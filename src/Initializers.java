197edca10200e8214461da6810d9f004ca14f6d9
Anand Bharti <anandbharti16012000@gmail.com>
2021-06-22 19:52:51 +0530

    22/06 19:40

---
+++ src/Initializers.java
@@ -0,0 +1,28 @@
+class Sample
+{
+	static {
+		System.out.println("static initializer");	
+		}
+	
+	{
+		System.out.println("non static initializer");
+	}
+
+
+}
+
+public class Initializers {
+
+	
+	
+	
+	public static void main(String[] args) {
+
+Sample ob1;
+Sample ob2;
+ob1=new Sample();
+ob2=new Sample();
+
+	}
+
+}
